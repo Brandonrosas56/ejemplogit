ce2f349bdd9c47c07c73931ed7efbbcfa9015c60
brandonrosas56 <brayanrosas635gmail.com>
2022-11-11 21:28:34 -0500

    primero

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<title> Menu</title>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body> <input type="checkbox" id="menu">
+    <nav> <label>SistematicCutex</label>
+        <ul>
+            <li><a href="#">Salir</a></li>
+        </ul> <label for="menu" class="menu-bar"> <i class="fa fa-bars"></i> </label>
+    </nav>
+    <div class="side-menu">
+        <br><br>
+        
+        <br> <a href="#"><i class="fa fa-user"></i>
+        <h1>aux. bodega</h1>
+        <a href="#"><i class="fa fa-shopping-basket"></i><span>Productos</span></a> 
+        <a href="#"><i class="fa fa-sellsy"></i><span>Usuarios</span></a>  
+    </div>
+</body>
+
+</html>
